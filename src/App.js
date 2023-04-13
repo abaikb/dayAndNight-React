0e39fcb4a97ab10874b7d00dec1b28ca7fae78ef
abaikb <abaikb@icloud.com>
2023-04-13 15:00:33 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [isDay, setIsDay] = useState(true)
+
+  const toggleMode = () => {
+    setIsDay(!isDay)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={isDay ? 'day' : 'night'}>
+        <div className="sun"></div>
+      </div>
+      <button onClick={toggleMode}>
+        {isDay ? 'Switch to Night Mode' : 'Switch to Day Mode'}
+      </button>
+    </>
+  )
 }
 
 export default App;
